b088f1144f1393550e3d21006a8866cdc07d7f9e
Jennifer Fisher <jennifer.fisher@oregonstate.edu>
2019-06-04 18:03:13 -0700

    trying to get the loop working...

--- Code/PlotBioPhysicalDataFromTheDatabase.R
+++ Code/PlotBioPhysicalDataFromTheDatabase.R
@@ -10,12 +10,16 @@ rm(list=ls())
 #library(RODBC)
 #library(doBy)
 library(pastecs)
-library(lubridate) # for working with dates
-library(ggplot2)  # for creating graphs
-library(scales)   # to access breaks/formatting functions
-library(gridExtra) # for arranging plots
+library(lubridate)  # for working with dates
+library(ggplot2)    # for creating graphs
+library(scales)     # to access breaks/formatting functions
+library(gridExtra)  # for arranging plots
 library(plyer)
 library(dplyr)
+library(stinepack)  #time-series anz
+library(lubridate)  #for working with dates
+library(ggfortify)
+library(zoo)        #working with time series
 
 
 # db<-file.path("Z:/zooplank/Database/ZoopLab_FE_JenniferFisher.accdb") #connect database.
@@ -45,7 +49,7 @@ library(dplyr)
 setwd("C:/_JF/HMSC/Data/Limacina/R_data")
 getwd()
 
-data <-read.csv("qryNoSoBiomass_Richness_w_BasinScaleIndices.csv",header=TRUE)
+data <- read.csv("qryNoSoBiomass_Richness_w_BasinScaleIndices.csv",header=TRUE)
 
 # get the start and end dates of the data
 start.yr = min(data$YYYY)
@@ -54,78 +58,69 @@ start.mo = min(data$MM)
 end.yr = max(data$YYYY)
 end.mo = max(data$MM)
 
-#####
-#loop through all the variablese and calculate 3mo running means
-#####
-
-###use this moving average which is a window of averages centered on a middle point with an order = to half your periodicity
-library(pastecs)
-
 size <- dim(data)
 no.col <- ncol(data)
 no.row <- nrow(data)
 
 
+#####
+#loop through all the variablese and calculate 3mo running means
+#####
+
 data.3mo <- matrix(NA, no.row, no.col) #create matrix to store output in
 colnames(data.3mo) <- colnames(data)  #name the columns as the data
 
-#turn this into a loop once I get 1 column to work.....
-#seems best to get the 3mo avg then turn each var into a ts before plotting
 #only do for the bio data so start at 7
-library(stinepack)
+#for(i in 7:no.col){
+  for(i in 7:8){
 
-######after all this I think it is easier to create the 3mo running mean and time series variable for each plot...
-##### oh you thought it would be so easy....
-for(i in 7:no.col){
-
-  #data.3mo[i] <- decaverage(data[ ,i], order = 6)
-  #avg <- decaverage(.ts[], order = 6)
-  #avg <- decaverage(data[ ,i], order = 6) #this creates a time series variable
-
- 
+  
   #turn your data into a timeseries 
   data.ts <- ts(data[ ,i], start = c(start.yr,start.mo), end = c(end.yr,end.mo), frequency = 12)
   
-   #interpolate over NaNs
-  #from library(stinepack)
-  data.nonan = na.stinterp(data.ts)
+  data.nonan <- na.approx(data.ts, na.rm = TRUE)
   
+  #interpolate over NaNs
+  #data.nonan <- na.stinterp(data.ts)
   
-  avg <- decaverage(data.nonan, order = 1) # 2 * order + 1 
-  avg.series <- (avg$series[ ,1]) #1 are the smoothed data, 2 are the residuals
-
-  #avg <- filter(data.nonan,c(1,1,1)/3, method = "convolution",sides = 2)
-  #avg.series <- (avg$series[ ,1]) #1 are the smoothed data, 2 are the residuals
-  #avg.series.num <- as.numeric(avg.series)   #change from time series to numeric
-  #data.3mo[ ,i] <- (avg.series.num)   #stuff it back into the blank matrix
-  #avg.num <- as.numeric(avg)   #stuff it back into the blank matrix
-  data.3mo[ ,i] <- (avg.series)   #stuff it back into the blank matrix
+  #make sure you are using the correct filter function from the stats package
+  filter <- stats::filter
+  
+  #compute the 3mo running mean using filter
+  var.3mo <- filter(data.nonan, c(1,1,1)/3, method="convolution", circular = FALSE, sides=2)
+  
+  #stuff it back into the blank matrix as numeric (not ts object)
+  data.3mo[ ,i] <- as.numeric(var.3mo)   
+  
 }
 
-data.3mo[ ,1] <- (data$SigmaPlotDate) #stuff it back into the blank matrix
-data.3mo[ ,2] <- (data$YYYY)   #stuff it back into the blank matrix
-data.3mo[ ,3] <- (data$MM)   #stuff it back into the blank matrix
-
-####
-# Let's try filter to also compute a moving average and compare it to decaverage
-
-#ts objects cannot have nans
-data.ONI.nonan = na.stinterp(data$ONI)
-
-#turn your data into a timeseries 
-ONI.ts <- ts(data.ONI.nonan[], start = c(start.yr,start.mo), end = c(end.yr,end.mo), frequency = 12)
-
-#now apply the 3mo running average using 2 methods
-ONIfilt <- filter(ONI.ts, c(1,1,1)/3, method="convolution", sides=2)
-ONIdecavg <- decaverage(ONI.ts,order = 1)
-ONIdecavg.series <- (ONIdecavg$series[ ,1]) #1 are the smoothed data, 2 are the residuals
+#put the rest of the data in the matrix
+data.3mo[ ,1] <- (data$SigmaPlotDate) 
+data.3mo[ ,2] <- (data$YYYY)   
+data.3mo[ ,3] <- (data$MM)   
 
 
-plot(ONIfilt,ONIdecavg.series)
-#they are the same
 
-#run a correlation betwen 2 time series just to test
-cor.test(ONIfilt,ONIdecavg.series,method="pearson")  #corr coeff is 1
+# ####
+# # Let's compare filter and decaverage to compute 3mo running means
+# ####
+# 
+# #ts objects cannot have nans
+# data.ONI.nonan = na.stinterp(data$ONI)
+# 
+# #turn your data into a timeseries 
+# ONI.ts <- ts(data.ONI.nonan[], start = c(start.yr,start.mo), end = c(end.yr,end.mo), frequency = 12)
+# 
+# #now apply the 3mo running average using 2 methods
+# ONIfilt <- filter(ONI.ts, c(1,1,1)/3, method="convolution", sides=2)
+# ONIdecavg <- decaverage(ONI.ts,order = 1)
+# ONIdecavg.series <- (ONIdecavg$series[ ,1]) #1 are the smoothed data, 2 are the residuals
+# 
+# plot(ONIfilt,ONIdecavg.series)
+# #they are the same
+# 
+# #run a correlation betwen 2 time series just to test
+# cor.test(ONIfilt,ONIdecavg.series,method="pearson")  #corr coeff is 1
 
 
 
@@ -133,66 +128,53 @@ cor.test(ONIfilt,ONIdecavg.series,method="pearson")  #corr coeff is 1
 # okay now let's make some plots
 #####
 
-library(lubridate) # for working with dates
-library(ggplot2)  # for creating graphs
-library(gridExtra) #organize multiple plots
-library(ggfortify)
-library(stinepack)
-library(dplyr)
-
-# Add a column with your condition for the colored anomalies
-data <- data %>% mutate(mycolor = ifelse(y>0, "type1", "type2"))
 
 #interpolate over the nas
 no.cope.nonan = na.stinterp(data$NorthernBiomassAnomaly)
 #create a timeseries object 
-no.cope.ts <- ts(no.cope.nonan, start = c(start.yr,start.mo), end = c(end.yr,end.mo), frequency = 12)
+#no.cope.ts <- ts(no.cope.nonan, start = c(start.yr,start.mo), end = c(end.yr,end.mo), frequency = 12)
 #now apply the 3mo running average 
-no.cope.3mo <- filter(no.cope.ts, c(1,1,1)/3, method="convolution", sides=2)
+no.cope.3mo <- filter(no.cope.nonan, c(1,1,1)/3, method="convolution", sides=2)
 no.cope.3mo.num <- as.numeric(no.cope.3mo)
+data <- data %>% mutate(no.cope.3mo.num = no.cope.3mo.num, na.rm = FALSE)
 
-y.pos <- ifelse(data$NorthernBiomassAnomaly>0, data$NorthernBiomassAnomaly, 0)
-y.neg <- ifelse(data$NorthernBiomassAnomaly<0, data$NorthernBiomassAnomaly, 0)
 
 #deal with the dates
 data <- data %>% mutate(date = as.Date(data$SigmaPlotDate, format = '%m/%d/%Y'))
-min.date <- min(data$date)
-max.date <- max(data$date)
+min.date <- min(data$date) - 30   #pad the start date by 15 days
+max.date <- max(data$date) + 30   #pad the end date by 15 days
 
-# Add a column with your condition for the color
-data <- data %>% mutate(mycolor = ifelse(data$NorthernBiomassAnomaly>0, "pos", "neg"))
+####Plot Nortern and Southern biomass anomalies
 
+#add a column with your condition for the color
+data <- data %>% mutate(NoSo.color = ifelse(data$NorthernBiomassAnomaly>0, "pos", "neg"))
 
-
-###trying to get the anomalies colored...NOT WORKING in GGPLOT
-anom.colors <- c("#ff3db7", "#4699dd")
-
-
+#trying to get the anomalies colored
+anom.colors <- c("indianred3", "royalblue3")
 
 #now plot with ggplot
 pl.no.copes <- ggplot(data = data, aes(x = date, y = NorthernBiomassAnomaly, fill = mycolor)) + #fill color based on pos or neg anoms
-  geom_bar(stat = "identity", color = "black", na.rm = TRUE) + #make the bar outline black
-  scale_color_manual(values = anom.colors) +
+  geom_bar(stat = "identity", color = "black", size=0.05, na.rm = TRUE) + #make the bar outline black
+  scale_fill_manual(values = anom.colors) +
     theme_light() +
   theme(
   legend.position = "none",
   #panel.border = element_blank(),
   panel.grid.major.y = element_blank(),
-  panel.grid.minor.y = element_blank()
+  panel.grid.minor.y = element_blank(),
+  panel.grid.minor.x = element_blank()
   ) +
-  scale_x_date(limits = (c(min.date,max.date)), date_breaks = "2 years"
-               #, labels = "%y" #this is supposed to format the yy but there's a conflict with date_breaks??
-               )+               
-  #xlim(min(data$date),max(data$date))+
-  ggtitle("Northern Copepod Biomass")+
+    scale_x_date(limits = as.Date(c(min.date,max.date)), 
+    date_breaks = "1 years",
+    labels = date_format("%y") 
+               ) +  
+  coord_cartesian(xlim = NULL, ylim = NULL, expand = FALSE) +  #gets rid of extra white space
+                  
+  ggtitle("Northern Copepod Biomass") +
   xlab("Year") +
   ylab("Monthly biomass anomaly (Log10 C m-3)")
 
 
-
-
-y.pos <- ifelse(no.cope.3mo>0, no.cope.3mo, 0)
-y.neg <- ifelse(no.cope.3mo<0, no.cope.3mo, 0)
 
 autoplot(y.pos, ts.geom = 'bar', fill = 'red')
 autoplot(y.neg, ts.geom = 'ribbon', fill = 'dodgerblue4')
