a70f56d7b3585abf9d9e6a72fb21a0650b1b4111
Jennifer Fisher <jennifer.fisher@oregonstate.edu>
2019-06-07 19:04:19 -0700

    3mo mean loop is working. Combined data frames. Working towards making nicer plots

--- Code/PlotBioPhysicalDataFromTheDatabase.R
+++ Code/PlotBioPhysicalDataFromTheDatabase.R
@@ -68,12 +68,10 @@ no.row <- nrow(data)
 #####
 
 data.3mo <- matrix(NA, no.row, no.col) #create matrix to store output in
-colnames(data.3mo) <- colnames(data)  #name the columns as the data
+colnames(data.3mo) <- paste(colnames(data),"3mo", sep="")  #name the columns as the data
 
 #only do for the bio data so start at 7
 for(i in 7:no.col){
-  # for(i in 7:8){
-
   
   #turn your data into a timeseries 
   data.ts <- ts(data[ ,i], 
@@ -81,11 +79,9 @@ for(i in 7:no.col){
                 end = c(end.yr,end.mo), 
                 frequency = 12)
   
+  #interpolate over NaNs
   data.nonan <- na.approx(data[,i],
                           na.rm=FALSE)
-  
-  #interpolate over NaNs
-  #data.nonan <- na.stinterp(data.ts)
   
   #make sure you are using the correct filter function from the stats package
   filter <- stats::filter
@@ -95,16 +91,23 @@ for(i in 7:no.col){
   
   #stuff it back into the blank matrix as numeric (not ts object)
   data.3mo[ ,i] <- as.numeric(var.3mo)   
-  
 }
 
-#put the rest of the data in the matrix
-data.3mo[ ,1] <- (data$SigmaPlotDate) 
-data.3mo[ ,2] <- (data$YYYY)   
-data.3mo[ ,3] <- (data$MM)  
-data.3mo[ ,3] <- (data$MM)
-data.3mo[ ,3] <- (data$MM)
-data.3mo[ ,3] <- (data$MM)
+#turn it into a data frame which is easier to deal with?
+data.3mo <- as.data.frame(data.3mo)
+
+#combine the running means with the original data
+data <- bind_cols(data[1:no.col],data.3mo[7:no.col])
+
+# #put the rest of the data in the matrix
+# data.3mo[ ,1] <- (data$SigmaPlotDate) 
+# data.3mo[ ,2] <- (data$YYYY)   
+# data.3mo[ ,3] <- (data$MM)  
+# data.3mo[ ,4] <- (data$NPGO)
+# data.3mo[ ,5] <- (data$PDO)
+# data.3mo[ ,6] <- (data$ONI)
+
+
 
 #if you want to plot everything use matplot (matrix plot)
 #matplot(data.3mo, type="l")
@@ -137,34 +140,30 @@ data.3mo[ ,3] <- (data$MM)
 #####
 
 
-#interpolate over the nas
-no.cope.nonan = na.stinterp(data$NorthernBiomassAnomaly)
-#create a timeseries object 
-#no.cope.ts <- ts(no.cope.nonan, start = c(start.yr,start.mo), end = c(end.yr,end.mo), frequency = 12)
-#now apply the 3mo running average 
-no.cope.3mo <- filter(no.cope.nonan, c(1,1,1)/3, method="convolution", sides=2)
-no.cope.3mo.num <- as.numeric(no.cope.3mo)
-data <- data %>% mutate(no.cope.3mo.num = no.cope.3mo.num, na.rm = FALSE)
-
 
 #deal with the dates
 data <- data %>% mutate(date = as.Date(data$SigmaPlotDate, format = '%m/%d/%Y'))
-min.date <- min(data$date) - 30   #pad the start date by 15 days
-max.date <- max(data$date) + 30   #pad the end date by 15 days
+#data.3mo <- data.3mo %>% mutate(date = as.Date(data.3mo$SigmaPlotDate3mo, format = '%m/%d/%Y'))
+
+min.date <- min(data$date) - 30   #pad the start date by 30 days
+max.date <- max(data$date) + 30   #pad the end date by 30 days
+
 
 ####Plot Nortern and Southern biomass anomalies
 
 #add a column with your condition for the color
-data <- data %>% mutate(NoCop.color = ifelse(data$NorthernBiomassAnomaly>0, "pos", "neg"))
+data <- data %>% mutate(NoCop.color = ifelse(data$NorthernBiomassAnomaly3mo>0, "pos", "neg"))
+data <- data %>% mutate(SoCop.color = ifelse(data$SouthernBiomassAnomaly3mo>0, "pos", "neg"))
 
 #trying to get the anomalies colored
 anom.color <- c("indianred3", "royalblue3")
 
 #now plot with ggplot
-pl.no.copes <- ggplot(data = data, aes(x = date, y = NorthernBiomassAnomaly, fill = NoCop.color)) + #fill color based on pos or neg anoms
-  geom_bar(stat = "identity", color = "black", size=0.05, na.rm = TRUE) + #make the bar outline black
-  scale_fill_manual(values = anom.color) +
-    theme_light() +
+pl.no.copes <- ggplot(data = data, aes(x = date, y = NorthernBiomassAnomaly3mo, fill = NoCop.color)) + #fill color based on pos or neg anoms
+  geom_bar(stat = "identity", color = "black", size=0.05, na.rm = TRUE) +  #make the bar outline black
+  scale_fill_manual(values = anom.color) +  #color the anoms w/ what you want
+  
+  theme_light() +
   theme(
   legend.position = "none",
   #panel.border = element_blank(),
@@ -172,87 +171,70 @@ pl.no.copes <- ggplot(data = data, aes(x = date, y = NorthernBiomassAnomaly, fil
   panel.grid.minor.y = element_blank(),
   panel.grid.minor.x = element_blank()
   ) +
-    scale_x_date(limits = as.Date(c(min.date,max.date)), 
-    date_breaks = "1 years",
-    labels = date_format("%y") 
-               ) +  
+    
+  scale_x_date(limits = as.Date(c(min.date.3mo,max.date.3mo)), 
+  date_breaks = "1 years",
+  labels = date_format("%y")) +
+ 
   coord_cartesian(xlim = NULL, ylim = NULL, expand = FALSE) +  #gets rid of extra white space
-                  
+  
   ggtitle("Northern Copepod Biomass") +
+  theme(plot.title = element_text(size = 12)) +
+  xlab("Year") +
+  ylab("Monthly biomass anomaly (Log10 C m-3)") 
+  
+  geom_point(data = data, aes(x = date, y = NorthernBiomassAnomaly), color = "black") +
+  geom_ribbon(data = data, aes(x = date, y = NorthernBiomassAnomaly), color = "black")
+
+
+
+
+
+pl.so.copes <- ggplot(data = data, aes(x = date, y = SouthernBiomassAnomaly3mo, fill = SoCop.color)) + #fill color based on pos or neg anoms
+  geom_bar(stat = "identity", color = "black", size=0.05, na.rm = TRUE) +  #make the bar outline black
+  scale_fill_manual(values = rev(anom.color)) +  #color the anoms w/ what you want
+  
+  theme_light() +
+  theme(
+    legend.position = "none",
+    #panel.border = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.minor.x = element_blank()
+  ) +
+  
+  scale_x_date(limits = as.Date(c(min.date.3mo,max.date.3mo)), 
+               date_breaks = "1 years",
+               labels = date_format("%y")) +
+  
+  coord_cartesian(xlim = NULL, ylim = NULL, expand = FALSE) +  #gets rid of extra white space
+  
+  ggtitle("Southern Copepod Biomass") +
+  theme(plot.title = element_text(size = 12)) +
   xlab("Year") +
   ylab("Monthly biomass anomaly (Log10 C m-3)")
 
 
 
-autoplot(y.pos, ts.geom = 'bar', fill = 'red')
-autoplot(y.neg, ts.geom = 'ribbon', fill = 'dodgerblue4')
-
-#try plotting with library(ggfortify)
-autoplot(no.cope.3mo, ts.geom = 'bar', fill = 'dodgerblue4')
-
-ggplot(data, aes(x = SigmaPlotDate, y = SouthernBiomassAnomaly)) +
-  geom_ribbon(stat = "identity", color = "red", size=0.75, na.rm = TRUE)
-
-
-pl.so.copes <- ggplot(data, aes(x = SigmaPlotDate, y = SouthernBiomassAnomaly)) +
+pl.PDO <- ggplot(data, aes(x = date, y = PDO)) +
   geom_bar(stat = "identity", color = "red", size=0.75, na.rm = TRUE)
 
-pl.PDO <- ggplot(data, aes(x = SigmaPlotDate, y = PDO)) +
-  geom_bar(stat = "identity", color = "red", size=0.75, na.rm = TRUE)
-
-pl.ONI <- ggplot(data, aes(x = SigmaPlotDate, y = ONI)) +
+pl.ONI <- ggplot(data, aes(x = date, y = ONI)) +
   geom_bar(stat = "identity", color = "red", size=0.75, na.rm = TRUE)
 
 # Show the plots on the same page
-grid.arrange(pl.so.copes, pl.no.copes, pl.ONI, pl.PDO, ncol = 1, nrow = 4)
+final.plots <- grid.arrange(pl.so.copes, pl.no.copes, pl.ONI, pl.PDO, ncol = 1, nrow = 4)
+
+#save the plot
+ggsave("BiophysicalVars_3mo.png", plot = final.plots, device = "png", path = "C:/_JF/HMSC/Data/Limacina/Figures",
+       width = 8.5, height = 10, units = c("in"),
+       dpi = 300)
 
 
 
 
-#######to make variable color plots- i.e. colored anomalies
-#from https://www.r-graph-gallery.com/302-lollipop-chart-with-conditional-color/
-
-# Create data (this takes more sense with a numerical X axis)
-x=seq(0, 2*pi, length.out=100)
-data=data.frame(x=x, y=sin(x) + rnorm(100, sd=0.2))
-
-# Add a column with your condition for the color
-data=data %>% mutate(mycolor = ifelse(y>0, "type1", "type2"))
-
-# plot
-ggplot(data, aes(x=x, y=y)) +
-  geom_segment( aes(x=x, xend=x, y=0, yend=y, color=mycolor), size=1.3, alpha=0.9) +
-  theme_light() +
-  theme(
-    legend.position = "none",
-    panel.border = element_blank(),
-  ) +
-  xlab("") +
-  ylab("Value of Y")
 
 
-######this code is supposed to put ggplot2 graphs together to make them look good-
-# from https://www.r-graph-gallery.com/261-multiple-graphs-on-same-page/ 
-
-
-# libraries
-library(ggplot2)
-library(gridExtra)
-
-# Make 3 simple graphics:
-g1=ggplot(mtcars, aes(x=qsec)) + geom_density(fill="slateblue")
-g2=ggplot(mtcars, aes(x=drat, y=qsec, color=cyl)) + geom_point(size=5) + theme(legend.position="none")
-g3=ggplot(mtcars, aes(x=factor(cyl), y=qsec, fill=cyl)) + geom_boxplot() + theme(legend.position="none")
-g4=ggplot(mtcars , aes(x=factor(cyl), fill=factor(cyl))) +  geom_bar()
-
-# Show the 4 plots on the same page
-grid.arrange(g1, g2, g3, g4, ncol=2, nrow =2)
-
-# Plots
-grid.arrange(g2, arrangeGrob(g3, g4, ncol=2), nrow = 2)
-grid.arrange(g1, g2, g3, nrow = 3)
-grid.arrange(g2, arrangeGrob(g3, g4, ncol=2), nrow = 1)
-grid.arrange(g2, arrangeGrob(g3, g4, nrow=2), nrow = 1)
 
 
 
